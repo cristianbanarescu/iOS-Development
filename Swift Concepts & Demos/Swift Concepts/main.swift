8ff8878dbc8c512aa718505ed85be8e5f44a48e7
Cristian Banarescu <cristianbanarescu@gmail.com>
2022-04-28 17:43:12 +0300

    Add content inside Naming Constants and Variables section

--- Swift Concepts & Demos/Swift Concepts/main.swift
+++ Swift Concepts & Demos/Swift Concepts/main.swift
@@ -21,7 +21,7 @@ var x = 0.0, y = 0.0, z = 0.0 // anothery way of declaring variables; doing it o
 // MARK: - Type annotations
 
 /// you can provide a type for a variable or constant to make sure that the values you provide for storing will be of an exact type
-/// type can be inferred from a variable/constant when you declare it and also give it an inital value, without explicitly writing the type 
+/// type can be inferred from a variable/constant when you declare it and also give it an inital value, without explicitly writing the type
 
 var welcomeMessage: String // declare a variable of type 'String'; can store any String value
 welcomeMessage = "Hello"
@@ -30,3 +30,29 @@ var red, green, blue: Double // use only 1 line to declare 3 constants with the 
 
 // MARK: - Naming Constants and Variables
 
+/// Constans and variables can contain almost any character in their name.
+/// There are some restrictions though. They can't:
+///   - begin with a number
+///   - contain whitespace characters, mathematical symbols, arrows, private-use Unicode scalar values, or line- and box-drawing characters.
+///
+/// For more on naming restrictions, please see Naming section from: https://docs.swift.org/swift-book/LanguageGuide/TheBasics.html
+
+let π = 3.14159
+let 你好 = "你好世界"
+let 🐶🐮 = "dogcow"
+
+/// You can't use the same name for a variable/constant and just change its type
+/// Nor you can declare it again with the same name
+var myVar: Int = 10
+var myVar: String = "9"
+
+var default = 9 // You can't directly name a variable/constant using direct Swift keywords
+
+// If you do want to name it using a Swift keyword, surround your variable/constant name with backticks (`)
+var `default` = 9
+
+var myVariable = 9
+myVariable = 10 // this is OK, because myVariable is a variable, you can change its value after it was set
+
+let myConstant = 10
+myConstant = 11 // this is NOK, because myConstant is a constant, you cannot change its value after it was set
