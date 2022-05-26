bb16fd420354ffcfd02ac04341b7fd2ae70d7ba8
Cristian Banarescu <cristianbanarescu@gmail.com>
2022-05-26 16:59:16 +0300

    Added actual code inside ARC project.

--- Memory Management/ARC-HowItWorks/ARC-HowItWorks/main.swift
+++ Memory Management/ARC-HowItWorks/ARC-HowItWorks/main.swift
@@ -7,5 +7,17 @@
 
 import Foundation
 
-print("Hello, World!")
+class ARCMemoryManager {
+    deinit {
+        print("ARCMemoryManager deallocated from Memory")
+    }
+}
 
+var manager: ARCMemoryManager? = ARCMemoryManager() // ARC count = 1 (count of references to ARCMemoryManager)
+var otherManager: ARCMemoryManager? = manager // ARC count = 2 (count of references to ARCMemoryManager)
+var anotherManager: ARCMemoryManager? = manager // ARC count = 3 (count of references to ARCMemoryManager)
+
+otherManager = nil // ARC count = 2 (count of references to ARCMemoryManager)
+anotherManager = nil // ARC count = 1 (count of references to ARCMemoryManager)
+
+manager = nil // ARC Count = 0  (count of references to ARCMemoryManager)
