882a350ddf853cb0fac1b5153f68aed40b21d0a7
Cristian Banarescu <cristianbanarescu@gmail.com>
2023-02-21 16:34:51 +0200

    Created AccessibilityDemo folder together with its blank Xcode project

---
+++ Accessibility/AccessibilityDemo/AccessibilityDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AccessibilityDemo
+//
+//  Created by Cristian Banarescu on 21.02.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
