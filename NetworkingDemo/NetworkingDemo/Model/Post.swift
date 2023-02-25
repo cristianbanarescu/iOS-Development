b9cfc92df2b3897ae125e15e400a06fba6acfb82
Cristian Banarescu <cristianbanarescu@gmail.com>
2023-02-25 18:18:16 +0200

    Added implementation for the fetchPosts method. Made Post conform to Codable so Post objects can be decoded from the data obtained from the request.

--- NetworkingDemo/NetworkingDemo/Model/Post.swift
+++ NetworkingDemo/NetworkingDemo/Model/Post.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Post {
+struct Post: Codable {
     let userId: Int
     let id: Int
     let title: String
--- NetworkingDemo/NetworkingDemo/ViewController.swift
+++ NetworkingDemo/NetworkingDemo/ViewController.swift
@@ -10,6 +10,10 @@ import SwiftUI
 
 class ViewController: UIViewController {
     
+    // MARK: - Properties
+    
+    private var posts: [Post] = []
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
@@ -46,6 +50,45 @@ private extension ViewController {
     }
     
     func fetchPosts() {
+        let urlString = "https://jsonplaceholder.typicode.com/posts"
         
+        guard let url = URL(string: urlString) else {
+            return
+        }
+        
+        let urlRequest = URLRequest(url: url)
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            let dataTask = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+                if let error = error {
+                    print("Couldn't fetch data from \(urlString). Error: \(error.localizedDescription)")
+                }
+                
+                if let response = response as? HTTPURLResponse {
+                    print("Response with status code: \(response.statusCode)")
+                }
+                
+                if let data = data {
+                    do {
+                        let posts = try JSONDecoder().decode([Post].self, from: data)
+                        
+                        DispatchQueue.main.async { [weak self] in
+                            guard let self = self else {
+                                return
+                            }
+                            self.posts = posts
+                            self.reloadData()
+                        }
+                    } catch {
+                        print("Could not decode Post from data, error: \(error.localizedDescription)")
+                    }
+                }
+            }
+            dataTask.resume()
+        }
+    }
+    
+    func reloadData() {
+        // TODO 
     }
 }
