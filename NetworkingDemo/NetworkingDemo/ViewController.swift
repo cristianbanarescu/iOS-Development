9e16a602c24e4f0c972b80e429b923304fd987cc
Cristian Banarescu <cristianbanarescu@gmail.com>
2023-02-25 17:23:37 +0200

    Add PostsCollectionView inside ViewController

--- NetworkingDemo/NetworkingDemo/ViewController.swift
+++ NetworkingDemo/NetworkingDemo/ViewController.swift
@@ -6,11 +6,31 @@
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let hostingVC = UIHostingController(rootView: PostsCollectionView(numberOfPosts: 30))
+        
+        guard let swiftuiView = hostingVC.view else {
+            return
+        }
+        
+        swiftuiView.translatesAutoresizingMaskIntoConstraints = false
+        
+        addChild(hostingVC)
+        view.addSubview(swiftuiView)
+        
+        NSLayoutConstraint.activate([
+            swiftuiView.topAnchor.constraint(equalTo: view.topAnchor, constant: 10),
+            swiftuiView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10),
+            swiftuiView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10),
+            swiftuiView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -10)
+        ])
+        
+        hostingVC.didMove(toParent: self)
     }
 }
