7490edd1b422a234bb7c5a213cf74a2b49763445
Cristian Banarescu <cristianbanarescu@gmail.com>
2023-02-25 23:28:57 +0200

    Changed title for the ViewController to "My Posts". Added navigation bar buttons for: creating, deleting and updating posts. Added implementation for deleting a post. Added comments on how to implement PUT and POST requests.

--- NetworkingDemo/NetworkingDemo/ViewController.swift
+++ NetworkingDemo/NetworkingDemo/ViewController.swift
@@ -19,6 +19,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        title = "My posts"
+        setupNavigationBarButtons()
         fetchPosts()
     }
 }
@@ -49,18 +51,14 @@ private extension ViewController {
     }
     
     func fetchPosts() {
-        let urlString = "https://jsonplaceholder.typicode.com/posts"
-        
-        guard let url = URL(string: urlString) else {
+        guard let urlRequest = urlRequest(with: "https://jsonplaceholder.typicode.com/posts") else {
             return
         }
-        
-        let urlRequest = URLRequest(url: url)
         
         DispatchQueue.global(qos: .userInitiated).async {
             let dataTask = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
                 if let error = error {
-                    print("Couldn't fetch data from \(urlString). Error: \(error.localizedDescription)")
+                    print("Couldn't fetch data. Error: \(error.localizedDescription)")
                 }
                 
                 if let response = response as? HTTPURLResponse {
@@ -78,7 +76,7 @@ private extension ViewController {
                             self.posts = posts
                             self.setupPostsCollectionView()
                             // TODO do not recreate whole collection view;
-                            // just 'reload' the posts inside it 
+                            // just 'reload' the posts inside it
                         }
                     } catch {
                         print("Could not decode Post from data, error: \(error.localizedDescription)")
@@ -88,4 +86,77 @@ private extension ViewController {
             dataTask.resume()
         }
     }
+    
+    func setupNavigationBarButtons() {
+        let deletePostsButton = UIBarButtonItem(barButtonSystemItem: .trash, target: self, action: #selector(deletePosts))
+        let updatePostsButton = UIBarButtonItem(barButtonSystemItem: .edit, target: self, action: #selector(updatePostAction))
+        let createPostsButton = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(createPostAction))
+        
+        navigationItem.setLeftBarButtonItems([updatePostsButton, deletePostsButton], animated: true)
+        navigationItem.setRightBarButtonItems([createPostsButton], animated: true)
+    }
+    
+    @objc func deletePosts() {
+        guard let urlRequest = urlRequest(with: "https://jsonplaceholder.typicode.com/posts/1", httpMethod: "DELETE") else {
+            return
+        }
+        
+        DispatchQueue.global(qos: .userInitiated).async {
+            let dataTask = URLSession.shared.dataTask(with: urlRequest) { data, response, error in
+                if let error = error {
+                    print("Couldn't perform delete request. Error: \(error.localizedDescription)")
+                }
+                
+                if let response = response as? HTTPURLResponse {
+                    print("Response with status code: \(response.statusCode)")
+                }
+            }
+            dataTask.resume()
+        }
+    }
+    
+    @objc func updatePostAction() {
+        /*
+         fetch('https://jsonplaceholder.typicode.com/posts/1', {
+           method: 'PUT',
+           body: JSON.stringify({
+             id: 1,
+             title: 'foo',
+             body: 'bar',
+             userId: 1,
+           }),
+           headers: {
+             'Content-type': 'application/json; charset=UTF-8',
+           },
+         })
+         */
+    }
+    
+    @objc func createPostAction() {
+        /*
+         fetch('https://jsonplaceholder.typicode.com/posts', {
+           method: 'POST',
+           body: JSON.stringify({
+             title: 'foo',
+             body: 'bar',
+             userId: 1,
+           }),
+           headers: {
+             'Content-type': 'application/json; charset=UTF-8',
+           },
+         })
+         */
+    }
+    
+    func urlRequest(with urlString: String, httpMethod: String? = nil) -> URLRequest? {
+        guard let url = URL(string: urlString) else {
+            return nil
+        }
+        
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = httpMethod
+        
+        return urlRequest
+    }
+    
 }
