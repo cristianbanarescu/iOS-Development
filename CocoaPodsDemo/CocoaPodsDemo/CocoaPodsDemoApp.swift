e420e1747424917fb2dd7c8731d1a1ae9dcf1c37
Cristian Banarescu <cristianbanarescu@gmail.com>
2023-03-07 20:00:24 +0200

    Created project for showcasing the usage of cocoapods. Added lottie-ios as a pod.

---
+++ CocoaPodsDemo/CocoaPodsDemo/CocoaPodsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocoaPodsDemoApp.swift
+//  CocoaPodsDemo
+//
+//  Created by Cristian Banarescu on 07.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct CocoaPodsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
