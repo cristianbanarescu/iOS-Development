1214bef18869436f6978062544248d18c07cba50
Cristian Banarescu <cristianbanarescu@gmail.com>
2023-04-15 19:16:52 +0300

    Created CustomTableViewCell together with its xib file. Use CustomTableViewCell inside the TableViewController to display cells inside the tableView.

---
+++ TableViewUIKitDemo/TableViewUIKitDemo/CustomTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  CustomTableViewCell.swift
+//  TableViewUIKitDemo
+//
+//  Created by Cristian Banarescu on 15.04.2023.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    @IBOutlet weak var label: UILabel!
+}
