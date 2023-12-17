69cd634cd69b8db530e8df51deb0772e87de1015
Cristian Banarescu <cristianbanarescu@gmail.com>
2023-12-17 20:19:06 +0200

    Added template for project related to Swift opaque types.

---
+++ SwiftUIOpaqueTypesDemo/SwiftUIOpaqueTypes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIOpaqueTypes
+//
+//  Created by Cristian Banarescu on 17.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIOpaqueTypesDemo/SwiftUIOpaqueTypes/SwiftUIOpaqueTypesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIOpaqueTypesApp.swift
+//  SwiftUIOpaqueTypes
+//
+//  Created by Cristian Banarescu on 17.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIOpaqueTypesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
