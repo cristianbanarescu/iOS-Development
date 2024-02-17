96b2498829ef0e7de2376d62d500fd623a6509b7
Cristian Banarescu <cristianbanarescu@gmail.com>
2024-02-17 19:04:19 +0200

    Added small project about TextField and Toggle

---
+++ SwiftUI/SwiftUI-TextFieldDemo/SwiftUI-TextFieldDemo/ContentView.swift
@@ -0,0 +1,89 @@
+//
+//  ContentView.swift
+//  SwiftUI-TextFieldDemo
+//
+//  Created by Cristian Banarescu on 17.02.2024.
+//
+
+import SwiftUI
+
+/*
+ bind the TextField to a variable (via the TextField's init), in order to get or set the text on it
+ */
+
+struct ContentView: View {
+    @State private var textfieldText: String = ""
+    @State private var toggleOn: Bool = false
+    
+    var body: some View {
+        VStack {
+            // use the first param as the placeholder of the TextField
+            TextField("placeholder", text: $textfieldText)
+                .textFieldStyle(.roundedBorder)
+            
+            TextField("alignment leading", text: $textfieldText)
+                .textFieldStyle(.roundedBorder)
+                .multilineTextAlignment(.leading)
+            
+            TextField("alignment center", text: $textfieldText)
+                .textFieldStyle(.roundedBorder)
+                .multilineTextAlignment(.center)
+            
+            TextField("alignment trailing", text: $textfieldText)
+                .textFieldStyle(.roundedBorder)
+                .multilineTextAlignment(.trailing)
+            
+            TextField("Bigger font", text: $textfieldText)
+                .textFieldStyle(.roundedBorder)
+                .font(.largeTitle)
+            
+            // pay attention to NOT add a textFieldStyle on the TextField when customising it
+            TextField("Customised background", text: $textfieldText)
+                .padding()
+                .background(RoundedRectangle(cornerRadius: 90).foregroundStyle(.red.opacity(0.4)))
+            
+            TextField("Customised border", text: $textfieldText)
+                .padding()
+                .font(.headline)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 90).stroke(lineWidth: 9)
+                }
+            
+            ZStack(alignment: .leading) {
+                if textfieldText.isEmpty {
+                    Text("Custom placeholder")
+                        .bold()
+                        .foregroundStyle(.black)
+                        .italic()
+                        .padding(.leading, 10)
+                }
+                TextField("", text: $textfieldText)
+                    .padding(.leading, 10)
+                    .overlay {
+                        RoundedRectangle(cornerRadius: 90).stroke(lineWidth: 2)
+                    }
+            }
+            
+            TextField("Enter phone number", text: $textfieldText)
+                .padding()
+                .keyboardType(.phonePad)
+            
+            TextField("No autocorrection", text: $textfieldText)
+                .padding()
+                .autocorrectionDisabled()
+            
+            Toggle(isOn: $toggleOn, label: {
+                Text("Toggle textfield text")
+            })
+            .tint(.red)
+        }
+        .padding()
+        .onChange(of: toggleOn) {
+            textfieldText = toggleOn ? "Random" : ""
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI/SwiftUI-TextFieldDemo/SwiftUI-TextFieldDemo/SwiftUI_TextFieldDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TextFieldDemoApp.swift
+//  SwiftUI-TextFieldDemo
+//
+//  Created by Cristian Banarescu on 17.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TextFieldDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
