ae47a8f1a3340311c1288c011e3e92faf2da0249
Cristian Banarescu <cristianbanarescu@gmail.com>
2024-06-25 17:14:13 +0300

    Refactored some code. Added some comments and link

--- SwiftUI/SwiftUI-CoreData/SwiftUI-CoreData/CoreDataService.swift
+++ SwiftUI/SwiftUI-CoreData/SwiftUI-CoreData/CoreDataService.swift
@@ -8,16 +8,26 @@
 import CoreData
 import SwiftUI
 
+/// Use this object together with its embedded NSManagedObjectContext in order to:
+///  - fetch data > viewContext.fetch(fetchRequest)
+///  - insert data > Item(context: viewContext)
+///  - delete data > viewContext.delete(item)
 class CoreDataService: ObservableObject {
     private var viewContext: NSManagedObjectContext
-    @Published var items: [Item]
+    @Published var items: [Item] = []
     
     static let shared = CoreDataService(viewContext: NSManagedObjectContext.current)
     
     private init(viewContext: NSManagedObjectContext) {
         self.viewContext = viewContext
         
-        let fetchRequest = NSFetchRequest<Item>(entityName: "Item")
+        getItems()
+    }
+    
+    // MARK: - Fetch data
+    
+    private func getItems() {
+        let fetchRequest: NSFetchRequest<Item> = Item.fetchRequest()
         fetchRequest.sortDescriptors = [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)]
         
         do {
@@ -28,6 +38,8 @@ class CoreDataService: ObservableObject {
         }
     }
     
+    // MARK: - Add/Insert data
+    
     func addItem() {
         withAnimation {
             let newItem = Item(context: viewContext)
@@ -35,17 +47,12 @@ class CoreDataService: ObservableObject {
             
             items.append(newItem)
 
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+            saveContext()
         }
     }
 
+    // MARK: - Delete data
+    
     func deleteItems(offsets: IndexSet) {
         withAnimation {
             offsets.map { items[$0] }.forEach { item in
@@ -55,17 +62,25 @@ class CoreDataService: ObservableObject {
                 viewContext.delete(item)
             }
 
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
+            saveContext()
+        }
+    }
+    
+    // MARK: - Save state of context
+    fileprivate func saveContext() {
+        do {
+            try viewContext.save()
+        } catch {
+            // Replace this implementation with code to handle the error appropriately.
+            // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+            let nsError = error as NSError
+            fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
         }
     }
 }
+
+// NSPersistentContainer > NSManagedObjectContext
+// For graphical representation, see https://developer.apple.com/documentation/coredata/core_data_stack
 
 extension NSManagedObjectContext {
     static var current: NSManagedObjectContext {
