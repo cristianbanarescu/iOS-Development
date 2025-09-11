58abccd546b03ab48065401c7f658dfa5c5c2913
Cristian Banarescu <cristianbanarescu@gmail.com>
2025-09-11 16:13:51 +0300

    Adds a folder called 100 days SwiftUI into the SwiftUI folder. Creates a mock project to implement the first SwiftUI project - WeSplit.

---
+++ SwiftUI/100 days SwiftUI/WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Cristian Banarescu on 11.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
